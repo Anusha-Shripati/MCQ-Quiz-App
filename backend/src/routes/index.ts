13852a80dc189361295af3d2d5b073b69c760b61
Akash Kanani <akash@logicrays.com>
2025-01-16 08:28:17 +0000

    Development

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,9 +3,11 @@ import express from "express";
 
 //local imports
 import articleRouter from "./articles.router";
+import adminRouter from "./user.router";
 
 const router = express.Router();
 
 router.use("/articles", articleRouter);
+router.use("/user", adminRouter);
 
 export default router;

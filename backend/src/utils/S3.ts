d8c0ed5e477aa64f3f16af93e3506f855b0a970a
kunj_lr_mern <kunj+git@logicrays.com>
2025-07-02 13:20:06 +0530

    add region in default

--- backend/src/utils/S3.ts
+++ backend/src/utils/S3.ts
@@ -5,5 +5,5 @@ export const s3Client = new S3Client({
       accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
     },
-    region: process.env.AWS_REGION as string,
+    region: process.env.AWS_REGION as string  || 'ap-south-1',
   });

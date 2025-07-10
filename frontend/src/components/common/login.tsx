0a33990f3b58b9058eac625805cb039269bbc42a
Megh <megh@logicrays.com>
2025-07-10 18:05:46 +0530

    Solve build error

--- frontend/src/components/common/login.tsx
+++ frontend/src/components/common/login.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
-import { TypographyH2 } from '@/styles/typography';
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/form/button';
